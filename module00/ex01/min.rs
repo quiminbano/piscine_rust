d76f9e867722312e041e665d0634cafdb39f4fa1
Carlos Orellana <corellana1988@gmail.com>
2024-07-21 11:44:08 +0300

    modified ex01

--- module00/ex01/min.rs
+++ module00/ex01/min.rs
@@ -9,3 +9,19 @@ pub fn	min(a: i32, b: i32) -> i32
 		b
 	}
 }
+
+fn	main()
+{
+	let mut a: i32;
+	let mut b: i32;
+
+	a = 2;
+	b = 4;
+	println!("The value of min between {} and {} is {}", a, b, min(a, b));
+	a = 5;
+	b = 5;
+	println!("The value of min between {} and {} is {}", a, b, min(a, b));
+	a = 6;
+	b = 3;
+	println!("The value of min between {} and {} is {}", a, b, min(a, b));
+}
